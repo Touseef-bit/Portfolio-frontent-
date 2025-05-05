e067b723d15dbd31aa1c25ff233cd4c3d76270e9
Touseef-bit <touseefali11092@gmail.com>
2025-05-05 23:14:23 +0500

    'first'

---
+++ server/app/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var logger = require('morgan');
+
+var app = express()
+const port = process.env.PORT || 3000
+const globalErrorHandler = require('../middleware/errorMiddleware')
+const emailRoutes = require('../routes/emailRoutes')
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// route
+
+app.get('/',(req,res)=>{
+  res.json({
+    message:"welcome to beckend"
+  })
+})
+
+app.use('/api/v1',emailRoutes)
+
+app.use(globalErrorHandler)
+
+
+const serverless = require('serverless-http');
+module.exports.handler = serverless(app);
