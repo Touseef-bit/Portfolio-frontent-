b08edbfd24a04c97cc6dc7d5c77a14ad15510675
Touseef-bit <touseefali11092@gmail.com>
2025-11-08 19:27:46 +0500

    updated cv

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  import tailwindcss from "@tailwindcss/vite"
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-  export default defineConfig({
-    plugins: [tailwindcss(), react(), flowbiteReact()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://beckend-jm29gn7oq-touseef-alis-projects.vercel.app/api',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [tailwindcss(), react(), flowbiteReact()],
+  build: {
+    outDir: 'build', // 👈 this changes the output folder name
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://beckend-jm29gn7oq-touseef-alis-projects.vercel.app/api',
+        changeOrigin: true,
+        secure: false,
       },
     },
-  })
+  },
+})
